cc9ba843232eb56cf9f03a91dd18cd50c69c5499
Yt027 <yayatangara027@gmail.com>
2025-11-09 18:26:52 +0000

    Updated datase installation infos

---
+++ configs/README.md
@@ -0,0 +1,12 @@
+# Database conigurations
+
+_In order to successfully connect this project to the sneakshop database which I assume you already created ([See main README.md file](../README.md)), you need to create a `configs/.env` file to store database and email credentials as below._
+     
+
+* DB_HOST=Your_Database_Host_Name # localhost in development mode
+* DB_NAME=Your_Database_Name
+* DB_USER=Your_Databse_Username
+* DB_PASS=Your_Database_Password
+
+* MAIL_HOST=Your_Mail_SMTP_Host_Address
+* MAIL_KEY=Your_Mail_Application_Password
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f355f81a49e59e8ccf20c694b2c6c94852f6f58f',
+        'reference' => 'd7551b525f2c25d8283b4ad06809d8ebe79edb1e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f355f81a49e59e8ccf20c694b2c6c94852f6f58f',
+            'reference' => 'd7551b525f2c25d8283b4ad06809d8ebe79edb1e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
